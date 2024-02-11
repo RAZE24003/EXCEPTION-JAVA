b5263f20493ac3ebc6f1b1d706e72eca6c262a55
SOUMADEEP CHAKRABORTY <109031209+RAZE24003@users.noreply.github.com>
2024-02-11 19:17:20 +0530

    Add files via upload

---
+++ B.java
@@ -0,0 +1,11 @@
+public class B {  
+  
+    public static void main(String[] args) {  
+          
+        int data=50/0; //may throw exception   
+          
+        System.out.println("rest of the code");  
+          
+    }  
+      
+} 
